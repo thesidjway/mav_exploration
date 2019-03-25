7e19f96a0d9b9142ef8640b3144697c0b227e530
Siddharth Jha <siddharthjha@outlook.com>
2019-03-25 18:18:58 +0530

    Initial run successful

--- include/mav_exploration/mav_exploration_node.h
+++ include/mav_exploration/mav_exploration_node.h
@@ -1,3 +1,14 @@
 #pragma once
 
+#include <thread>
+#include <chrono>
+
 #include <ros/ros.h>
+#include <ros/package.h>
+#include <tf/tf.h>
+#include <std_srvs/Empty.h>
+#include <trajectory_msgs/MultiDOFJointTrajectory.h>
+
+#include <mav_msgs/conversions.h>
+#include <mav_msgs/default_topics.h>
+#include <nbvplanner/nbvp_srv.h>
